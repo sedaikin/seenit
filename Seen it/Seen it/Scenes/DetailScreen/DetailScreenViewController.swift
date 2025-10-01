eb414e53be2211d028c5999cdda27b65b19af4fa
alexey.sedaikin <alexey.sedaikin@vk.team>
2025-10-01 18:52:19 +0300

    Fixes from last PR

--- Seen it/Seen it/Scenes/DetailScreen/DetailScreenViewController.swift
+++ Seen it/Seen it/Scenes/DetailScreen/DetailScreenViewController.swift
@@ -12,12 +12,10 @@ final class DetailScreenViewController: UIViewController {
     
     // MARK: - Properties
 
-    private let scrollView = UIScrollView()
-    private let contentView = UIView()
-    private lazy var detailScreenView = DetailScreenView()
     private let defaults = UserDefaultsKeys()
     private lazy var isTracked = defaults.containsMovieId(id, in: .tracked)
     private lazy var isWatched = defaults.containsMovieId(id, in: .watched)
+    private lazy var detailScreenView = DetailScreenView()
     private let viewModel: DetailScreenViewModel
     private var cancellables = Set<AnyCancellable>()
     private let id: Int
@@ -33,11 +31,15 @@ final class DetailScreenViewController: UIViewController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    override func loadView() {
+        view = detailScreenView
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         detailScreenView.delegate = self
-        
         setupBinding()
         setupUI()
         setupCustomBackButton()
@@ -47,51 +49,9 @@ final class DetailScreenViewController: UIViewController {
 
 private extension DetailScreenViewController {
     
-    // MARK: - Setup views
-    
-    func setupScrollView() {
-        scrollView.translatesAutoresizingMaskIntoConstraints = false
-        scrollView.showsVerticalScrollIndicator = false
-        view.addSubview(scrollView)
-        
-        NSLayoutConstraint.activate([
-            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
-            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-        ])
-        
-        scrollView.addSubview(contentView)
-        
-        contentView.translatesAutoresizingMaskIntoConstraints = false
-        let contentViewHeightAnchor = contentView.heightAnchor.constraint(equalTo: scrollView.heightAnchor)
-        NSLayoutConstraint.activate([
-            contentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
-            contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
-            contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
-            contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: -20),
-            contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
-        ])
-        
-        contentViewHeightAnchor.priority = .defaultLow
-        contentViewHeightAnchor.isActive = true
-    }
-    
-    private func setupDetailScreenView() {
-        contentView.addSubview(detailScreenView)
-        NSLayoutConstraint.activate([
-            detailScreenView.topAnchor.constraint(equalTo: contentView.topAnchor),
-            detailScreenView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
-            detailScreenView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
-            detailScreenView.heightAnchor.constraint(equalTo: contentView.heightAnchor)
-        ])
-    }
-    
     // MARK: - Setup UI
 
     func setupUI () {
-        setupScrollView()
-        setupDetailScreenView()
         view.backgroundColor = .background
     }
     
